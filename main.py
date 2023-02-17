b4f03c074e0a6cee9d7b1a089e2dbb201d8a8c50
SrimanPolusani <srimanpolusani1@gmail.com>
2023-02-17 15:52:00 +0530

    Add new data, remove unnecessary arguments in class, Add regularization in the algorithm and Change visualization code according to the new changes.

--- main.py
+++ main.py
@@ -4,59 +4,73 @@ import math
 
 
 class LogisticRegression:
-    def __init__(self, X, y, w_init, b_init, alpha, num_iters):
+    def __init__(self, txt_file, alpha, num_iters, lambda_):
         """
-        :param X: An ndarray(m,n) with m examples and n features
-        :param y: An ndarray(m) with m target values
-        :param w_init: An ndarray(n) representing n initial values of model param w for each feature
-        :param b_init: A scaler representing initial value of model param 'b'
-        :param alpha: Learning rate
-        :param num_iters: Total number of iterations in the gradient descent
+        :param txt_file: A txt file with training data
+        :param alpha: alpha: Learning
+        :param num_iters: Total number of iterations for the gradient descent
+        :param lambda_: Regularization parameter
         """
-        self.X_train = X
-        self.y_train = y
-        self.w = w_init
-        self.b = b_init
+        # Converts txt_file data into ndarray
+        self.X_train = np.loadtxt(txt_file, usecols=(0, 1), delimiter=',')
+        self.y_train = np.loadtxt(txt_file, usecols=2, delimiter=',')
+
+        self.m_examples = self.X_train.shape[0]  # Number of examples
+        self.n_features = self.X_train.shape[1]  # Number of features
         self.learning_rate = alpha
         self.total_iters = num_iters
+        self.reg_param = lambda_
 
-        # z and g(z) history and Cost [J(w, b)] history
-        self.z_g_history = {}
-        self.j_history = {}
+        # Generate initial params
+        np.random.seed(1)
+        self.w = np.random.randn(self.n_features)
+        self.b = np.random.randn()
 
         # To isolate all y = 1 examples and all y = 0 examples
         self.pos = self.y_train == 1
         self.neg = self.y_train == 0
 
-        self.no_of_ex = self.X_train.shape[0]  # Number of examples
-        self.no_of_features = self.X_train.shape[1]  # Number of features
+        # z and g(z) history and Cost [J(w, b)] history
+        self.z_g_history = {}
+        self.j_history = {}
 
     # <-----Cost Calculation----->
     def compute_cost_logistic(self):
         cost = 0
-        for ex_index in range(self.no_of_ex):
+        for ex_index in range(self.m_examples):
             zi = np.dot(self.w, self.X_train[ex_index]) + self.b
             sigmoid = 1 / (1 + np.exp(-zi))  # 1/1+(e^-z)
             self.z_g_history[zi] = sigmoid
             cost_one = -self.y_train[ex_index] * np.log(sigmoid)
-            cost_two = (1 - self.y_train[ex_index]) * np.log(1 - sigmoid)
+            cost_two = (1 - self.y_train[ex_index]) * np.log(1 - sigmoid + 1e-10)
             cost += cost_one - cost_two
-        cost = cost / self.no_of_ex
+        cost = cost / self.m_examples
+
+        # Regularization Part
+        reg_part = 0
+        for j in range(self.n_features):
+            reg_part += self.w[j] ** 2
+        reg_part = reg_part * ((self.reg_param / self.m_examples) * 2)
+
+        cost = cost + reg_part
         return cost
 
     # <-----dj_dw, dj_db Calculation----->
     def compute_gradient(self):
-        dj_dw = np.zeros(self.no_of_features)
+        dj_dw = np.zeros(self.n_features)
         dj_db = 0
-        for ex_index in range(self.no_of_ex):
+        for ex_index in range(self.m_examples):
             zi = np.dot(self.w, self.X_train[ex_index]) + self.b
             sigmoid = 1 / (1 + np.exp(-zi))
             err = sigmoid - self.y_train[ex_index]
-            for feature_num in range(self.no_of_features):
+            for feature_num in range(self.n_features):
                 dj_dw[feature_num] = dj_dw[feature_num] + (err * self.X_train[ex_index, feature_num])
             dj_db = dj_db + err
-        dj_dw = dj_dw / self.no_of_ex
-        dj_db = dj_db / self.no_of_ex
+        dj_dw = dj_dw / self.m_examples
+        dj_db = dj_db / self.m_examples
+
+        # Regularization Part
+        dj_dw += self.w * (self.reg_param / self.m_examples)
         return dj_dw, dj_db
 
     # <-----Performing Gradient Descent----->
@@ -90,9 +104,9 @@ class LogisticRegression:
         for axis_num, axis in enumerate(axes):
             x_data_pos = self.X_train[self.pos][0:, [axis_num]]
             x_data_neg = self.X_train[self.neg][0:, [axis_num]]
-            axis.scatter(x_data_pos, self.y_train[self.pos], marker='x', s=80, c='red', label='y=1', lw=3)
+            axis.scatter(x_data_pos, self.y_train[self.pos], marker='x', s=80, c='red', label='y=1', lw=1)
             axis.scatter(x_data_neg, self.y_train[self.neg], marker='o', s=100, facecolors='none', edgecolors='blue',
-                         lw=3)
+                         lw=1)
             axis.set_title(f'$X_{axis_num}$ Vs Y_train')
             axis.set_ylim(-0.08, 1.1)
             axis.set_xlabel(f'$X_{axis_num}$')
@@ -107,21 +121,17 @@ class LogisticRegression:
                      edgecolors='blue', lw=3)
         axes.plot([0, -self.b / self.w[0]], [-self.b / self.w[1], 0], color='#1AA7EC', lw=2)
         axes.set_title('$X_0$ ' + 'Vs ' + '$X_1$', fontsize=20)
-        axes.axis([0, 3.5, 0, 3.0])
         axes.set_xlabel('$X_0$', fontsize=14)
         axes.set_ylabel('$X_1$', fontsize=14)
         plt.show()
 
 
-X_tmp = np.array([[0.5, 1.5], [1, 1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
-y_tmp = np.array([0, 0, 0, 1, 1, 1])
-w_tmp = np.zeros(X_tmp.shape[1])
-b_tmp = 0.
-alph = 0.1
-iters = 10000
+alph = 0.001
+iters = 100_000
+lambda_value = 10
 
 # Instance of the object
-ml_object = LogisticRegression(X_tmp, y_tmp, w_tmp, b_tmp, alph, iters)
+ml_object = LogisticRegression('data1.txt', alph, iters, lambda_value)
 final_w, final_b, _ = ml_object.gradient_descent()
 print('Final value of w: {}\nFinal value of b: {}'.format(final_w, final_b))
 ml_object.visualize_sigmoid()
